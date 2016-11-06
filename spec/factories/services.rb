61072ed0a275146fb57a1eb14c55eda5281eda01
Chris Sun <christophersun@hotmail.com>
2016-11-05 19:36:14 -0700

    Added navbar and Service creation page

---
+++ spec/factories/services.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+  factory :service do
+    title "MyString"
+    description "MyString"
+    price 1
+    delivery_time 1
+    revisions 1
+    requirements "MyString"
+  end
+end
