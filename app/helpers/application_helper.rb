be1a1ebc7ceae1a203b6725b14b626780b39d18d
Chris Sun <christophersun@hotmail.com>
2017-03-15 15:58:54 -0700

    helper for gravatar, but unused

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,9 @@ module ApplicationHelper
   def app_name
     @app_name ||= Rails.application.class.to_s.split(':').first
   end
+
+  def avatar_url(user, size=150)
+    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+    "https://www.gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
+  end
 end
