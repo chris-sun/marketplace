6c392c81c8dc40ab441f01dd3c3eb3b3dd6c097e
Chris Sun <christophersun@hotmail.com>
2017-03-30 07:07:38 -0700

    tweak seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,19 +7,28 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 # Create 10 users
-10.times do |i|
-  email = "chrissun+testuser#{i}@gmail.com"
-  firstname = FFaker::Name.first_name
-  lastname = FFaker::Name.last_name
-  description = FFaker::Job.title
-  password = "abc#{i}def"
-  User.create(email: email,
-              firstname: firstname,
-              lastname: lastname,
-              description: description,
-              password: password,
-              password_confirmation: password)
+def add_users
+  10.times do |i|
+    email = "chrissun+testuser#{i}@gmail.com"
+    firstname = FFaker::Name.first_name
+    lastname = FFaker::Name.last_name
+    description = FFaker::Job.title
+    password = "abc#{i}def"
+    User.create(email: email,
+                firstname: firstname,
+                lastname: lastname,
+                description: description,
+                password: password,
+                password_confirmation: password)
+  end
 end
+
+def add_services
+  4
+end
+
+add_users if 0 == User.count
+add_services if 0 == Service.count
 
 # Create 10 services
 
